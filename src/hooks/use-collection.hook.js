bc6759a3715b749f33bdcbfca8cec5974a0811f7
TraXanh13 <kyleduong13@gmail.com>
2022-04-08 12:19:29 -0600

    Half way through mission 3

--- src/hooks/use-collection.hook.js
+++ src/hooks/use-collection.hook.js
@@ -1,8 +1,34 @@
+import { query } from '@onflow/fcl'
 import { useState } from 'react'
+import { useEffect } from 'react/cjs/react.production.min'
 
 export default function useCollection() {
-  const [loading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [collection, setCollection] = useState(false)
+
+  useEffect(() => {
+    if(!user?.addr)
+      return
+
+    const checkCollection = async () => {
+      try{
+        let res = await query({
+          cadence: `
+            import DappyContract from 0xDappy
+
+            pub fun main(addr: Address): Bool {
+              let ref = getAccount(addr).getCapability<&{DappyContract.CollectionPublic}>{/public/DappyCollection}
+              return ref
+            }
+          `
+        })
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    }
+    checkCollection();
+  }, [])
 
   const createCollection = async () => {
     setCollection(true)
