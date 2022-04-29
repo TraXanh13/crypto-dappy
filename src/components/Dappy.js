b7737c7c99f6fc2efb46b0b66d23b76859a757ea
TraXanh13 <kyleduong13@gmail.com>
2022-04-29 14:31:50 -0600

    revert dna back

--- src/components/Dappy.js
+++ src/components/Dappy.js
@@ -10,7 +10,7 @@ export default function Dappy({ dna = "FF5A9D.FFE922.60C5E5.0" }) {
   return (
     <svg className="wiggling" width="130" height="130" viewBox="0 0 530 530" fill="none" xmlns="http://www.w3.org/2000/svg" >
       <Body outline="white" />
-      <Stripes stripes={generateStripeColors("725735.605edf.aaaaaa.1")} />
+      <Stripes stripes={generateStripeColors(dna)} />
       <Eyes
         color={generateEyeColor(dna)}
         reflection="white"
